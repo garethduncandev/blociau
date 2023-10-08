a9ddc0d5c3bb49ab1795ac3c8d7450358c7f3aa1
Gareth Duncan <gareth.duncan@fft.org.uk>
2023-10-08 16:13:07 +0100

    Using default export for blocks.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/blocks.ts'),
       name: 'blocks',
       fileName: 'blocks',
     },
