efe2ee476d8087bc0b744a1f0e0899e9984d4bea
garethduncandev <garethduncandev@outlook.com>
2023-10-16 19:27:37 +0100

    Refactored into multiple files

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,13 +1,9 @@
-interface Column {
-  startX: number;
-  startY: number;
-  fill: boolean;
-  blockWidth: number;
-}
+import { Column } from './column';
+import { splitNumberIntoRandomNonRepeatingArray } from './helpers/arrays';
+import { createContext, isWhiteOrTransparent } from './helpers/canvas';
+import { createEmptySVGElement, createSvgElements } from './helpers/svg';
 
 export default class Blocks {
-  private previousClassName: string | undefined = undefined;
-
   public constructor(
     private blockHeight: number,
     private codeBlockMinWidth: number,
@@ -17,12 +13,12 @@ export default class Blocks {
   ) {}
 
   public create(id: string, image: HTMLImageElement): SVGSVGElement {
-    const context = this.createContext(image.width, image.height);
+    const context = createContext(image.width, image.height);
     context.drawImage(image, 0, 0);
     const rowsCount = image.height / this.blockHeight;
     const columnsCount = image.width / this.codeBlockMinWidth;
 
-    const result = this.createSVGRectElements(
+    const result = this.createBlocks(
       context,
       rowsCount,
       columnsCount,
@@ -30,46 +26,14 @@ export default class Blocks {
       this.codeBlockMaxWidth
     );
 
-    const outputSvg = this.createSVGElement(image.width, image.height, id);
+    const outputSvg = createEmptySVGElement(image.width, image.height, id);
 
     outputSvg.getElementById(`${id}-code-blocks-group`)?.append(...result);
 
     return outputSvg;
   }
 
-  private createSVGElement(
-    width: number,
-    height: number,
-    id: string
-  ): SVGSVGElement {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    svg.setAttribute('xmlns:svg', 'http://www.w3.org/2000/svg');
-    svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
-    const elementNS = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'g'
-    );
-    elementNS.setAttribute('id', `${id}-code-blocks-group`);
-    svg.appendChild(elementNS);
-    return svg;
-  }
-
-  private createContext(
-    canvasWidth: number,
-    canvasHeight: number
-  ): CanvasRenderingContext2D {
-    const canvas = document.createElement('canvas');
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    const context = canvas.getContext('2d', { willReadFrequently: true });
-    if (!context) {
-      throw new Error('Could not get context');
-    }
-    return context;
-  }
-
-  private createSVGRectElements(
+  private createBlocks(
     context: CanvasRenderingContext2D,
     rowsCount: number,
     columnsCount: number,
@@ -83,7 +47,7 @@ export default class Blocks {
     // work through each column
     for (let y = 0; y < rowsCount; y++) {
       // create svg elements
-      const svgRowColumnElements = this.createRowColumnSvgElements(
+      const svgRowColumnElements = this.createRowElements(
         context,
         columnsCount,
         startY,
@@ -97,18 +61,18 @@ export default class Blocks {
     return svgElements;
   }
 
-  private createRowColumnSvgElements(
+  private createRowElements(
     context: CanvasRenderingContext2D,
     columnsCount: number,
     startY: number,
     codeBlockMinWidth: number,
     codeBlockMaxWidth: number
   ): SVGRectElement[] {
-    let columns = this.splitRowIntoColumns(context, columnsCount, startY);
+    let columns = this.calculateColumns(context, columnsCount, startY);
 
     // merge filled columns next to each other together
     // this allows us to calculate the min and max length to work with
-    columns = this.mergeRowColumns(columns);
+    columns = this.mergeColumns(columns);
 
     // split columns into random length blocks for code effect
     columns = this.splitColumnsIntoRandomLengthColumns(
@@ -118,10 +82,16 @@ export default class Blocks {
     );
 
     // create svg elements
-    return this.createSvgElements(columns);
+    return createSvgElements(
+      columns,
+      this.blockHeight,
+      codeBlockMinWidth,
+      this.padding,
+      this.styleVariationsCount
+    );
   }
 
-  private splitRowIntoColumns(
+  private calculateColumns(
     context: CanvasRenderingContext2D,
     columnsCount: number,
     startY: number
@@ -129,7 +99,7 @@ export default class Blocks {
     let startX = 0;
     const columns: Column[] = [];
     for (let x = 0; x < columnsCount; x++) {
-      const whiteOrTransparent = this.whiteOrTransparent(
+      const whiteOrTransparent = isWhiteOrTransparent(
         context,
         startX,
         startY,
@@ -148,34 +118,34 @@ export default class Blocks {
     return columns;
   }
 
-  private mergeRowColumns(columns: Column[]): Column[] {
+  private mergeColumns(columns: Column[]): Column[] {
     const mergedColumns: Column[] = [];
 
     for (let x = 0; x < columns.length; x++) {
       const currentColumn = columns[x];
 
-      if (currentColumn.fill) {
-        let nextIndex = x + 1;
-        let nextColumn = columns[nextIndex];
-
-        while (
-          nextColumn &&
-          nextColumn.fill &&
-          nextColumn.startX === currentColumn.startX + currentColumn.blockWidth
-        ) {
-          currentColumn.blockWidth += nextColumn.blockWidth;
-          nextIndex++;
-          nextColumn = columns[nextIndex];
-        }
-
-        mergedColumns.push(currentColumn);
-        x = nextIndex - 1;
-      } else {
-        mergedColumns.push(currentColumn);
+      if (!currentColumn.fill) {
+        continue;
       }
+
+      let nextIndex = x + 1;
+      let nextColumn = columns[nextIndex];
+
+      while (
+        nextColumn &&
+        nextColumn.fill &&
+        nextColumn.startX === currentColumn.startX + currentColumn.blockWidth
+      ) {
+        currentColumn.blockWidth += nextColumn.blockWidth;
+        nextIndex++;
+        nextColumn = columns[nextIndex];
+      }
+
+      mergedColumns.push(currentColumn);
+      x = nextIndex - 1;
     }
 
-    return mergedColumns.filter((x) => x.fill);
+    return mergedColumns;
   }
 
   private splitColumnsIntoRandomLengthColumns(
@@ -187,7 +157,7 @@ export default class Blocks {
     for (let x = 0; x < columns.length; x++) {
       const blockWidth = columns[x].blockWidth;
 
-      const newBlockWidths = this.splitNumberIntoRandomNonRepeatingArray(
+      const newBlockWidths = splitNumberIntoRandomNonRepeatingArray(
         blockWidth,
         codeBlockMinWidth,
         codeBlockMaxWidth
@@ -210,164 +180,5 @@ export default class Blocks {
       }
     }
     return result;
-  }
-
-  private whiteOrTransparent(
-    context: CanvasRenderingContext2D,
-    startX: number,
-    startY: number,
-    blockWidth: number,
-    blockHeight: number
-  ): boolean {
-    const pixelData = context.getImageData(
-      startX,
-      startY + Math.floor(blockHeight / 2),
-      blockWidth,
-      1
-    ).data;
-
-    let whiteOrTransparent = true;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      const red = pixelData[i];
-      const green = pixelData[i + 1];
-      const blue = pixelData[i + 2];
-      const alpha = pixelData[i + 3];
-
-      // if 100% transparent, ignore even if it has color
-      if (alpha === 0) {
-        continue;
-      }
-
-      // if any color, then it's not white or transparent
-      if (red < 255 || green < 255 || blue < 255) {
-        whiteOrTransparent = false;
-        break;
-      }
-    }
-    return whiteOrTransparent;
-  }
-
-  private createSvgElements(columns: Column[]): SVGRectElement[] {
-    const rectangles: SVGRectElement[] = [];
-
-    for (let x = 0; x < columns.length; x++) {
-      const fill = columns[x].fill;
-      if (fill) {
-        const rect = this.createRectangle(
-          columns[x].startX,
-          columns[x].startY,
-          columns[x].blockWidth,
-          this.blockHeight,
-          this.styleVariationsCount
-        );
-        rectangles.push(rect);
-      }
-    }
-
-    return rectangles;
-  }
-
-  private createRectangle(
-    startX: number,
-    startY: number,
-    codeBlockWidth: number,
-    codeBlockHeight: number,
-    styleVariationsCount: number
-  ): SVGRectElement {
-    const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    rect.setAttribute('width', (codeBlockWidth - this.padding).toString());
-    rect.setAttribute('height', (codeBlockHeight - this.padding).toString());
-    rect.setAttribute('x', startX.toString());
-    rect.setAttribute('y', startY.toString());
-
-    const className = this.calculateRectClassName(
-      codeBlockWidth,
-      styleVariationsCount
-    );
-    rect.setAttribute('class', className);
-
-    return rect;
-  }
-
-  private calculateRectClassName(
-    blockWidth: number,
-    styleVariationsCount: number
-  ): string {
-    let className: string = '';
-
-    do {
-      const randomVariation =
-        Math.floor(Math.random() * styleVariationsCount) + 1;
-      className = `block-width-${Math.floor(
-        blockWidth / this.codeBlockMinWidth
-      )} block-variation-${randomVariation}`;
-      continue;
-    } while (this.previousClassName === className);
-    this.previousClassName = className;
-    return className;
-  }
-
-  private splitNumberIntoRandomNonRepeatingArray(
-    sum: number,
-    min: number,
-    max: number
-  ): number[] {
-    const maxArrayLength = Math.floor(sum / min);
-
-    const allPossibleValues = Array.from(Array(maxArrayLength).keys())
-      .map((m) => (m + 1) * min)
-      .filter((x) => x <= max);
-
-    let remainingSum = sum;
-    const result: number[] = [];
-    while (remainingSum > 0) {
-      const remainingPossibleValues = this.calculateRemainingPossibleValues(
-        result,
-        allPossibleValues,
-        remainingSum,
-        min
-      );
-
-      const randomIndex = Math.floor(
-        Math.random() * remainingPossibleValues.length
-      );
-      const randomNumber = remainingPossibleValues[randomIndex];
-
-      result.push(randomNumber);
-      remainingSum -= randomNumber;
-    }
-
-    return result;
-  }
-
-  private calculateRemainingPossibleValues(
-    currentResult: number[],
-    allPossibleValues: number[],
-    remainingSum: number,
-    min: number
-  ): number[] {
-    // Create new possible remaining values:
-    // 1. If we have no result yet, all values are possible
-    // 2. If possible values length is 1, then we use the existing possible values
-    // 3. If remaining sum equals min value, then we use the existing possible values
-    // 4. If 1 and 2 are not true then we
-    // 5. a) filter out the previous value
-    // 6. b) filter out all values that are larger than the remaining sum
-
-    if (currentResult.length === 0) {
-      return allPossibleValues;
-    }
-
-    if (allPossibleValues.length === 1) {
-      return allPossibleValues;
-    }
-
-    if (remainingSum === min) {
-      return [min];
-    }
-
-    return allPossibleValues
-      .filter((x) => x !== currentResult[currentResult.length - 1])
-      .filter((x) => x <= remainingSum);
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/blocks.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'blocks',
       fileName: 'blocks',
     },
