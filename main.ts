c463b653025f709dde22f426f82820ed4d9ac38b
garethduncandev <garethduncandev@outlook.com>
2023-10-23 20:36:02 +0100

    v2

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ const borderRadius = 2;
 const blockStyles: BlockStyle[] = [
   {
     width: codeBlockMinWidth,
-    color: 'red',
+    color: 'black',
     borderRadius: borderRadius,
   },
   {
@@ -18,17 +18,17 @@ const blockStyles: BlockStyle[] = [
   },
   {
     width: codeBlockMinWidth * 2,
-    color: 'yellow',
-    borderRadius: borderRadius,
-  },
-  {
-    width: codeBlockMinWidth * 3,
     color: 'green',
     borderRadius: borderRadius,
   },
   {
     width: codeBlockMinWidth * 3,
-    color: 'purple',
+    color: 'red',
+    borderRadius: borderRadius,
+  },
+  {
+    width: codeBlockMinWidth * 3,
+    color: 'brown',
     borderRadius: borderRadius,
   },
 ];
@@ -39,48 +39,6 @@ const img = document.getElementById('input') as HTMLImageElement;
 const output = document.getElementById('output');
 const svg = blocks.create('circle', img);
 
-// typing effect
-// todo - expose in library
-const style = document.createElement('style');
-document.head.appendChild(style);
-
-const keyframes = `
-@keyframes typing {
-  from {
-    width: 0;
-  }
-  to {
-    /* width: 100%; */
-    opacity: 1;
-  }
-}
-`;
-
-style.sheet?.insertRule(keyframes, style.sheet.cssRules.length);
-const rects = svg.getElementsByTagName('rect');
-
-let currentDelay = 0;
-let previousRect: SVGRectElement | undefined = undefined;
-const typingTime = 0.1;
-for (let i = 0; i < rects.length; i++) {
-  const rect = rects[i];
-
-  const previousWidth = previousRect
-    ? previousRect?.width.baseVal.value + padding
-    : 0;
-
-  const currentWidth = rect.width.baseVal.value + padding;
-
-  const delayTime =
-    currentDelay + (previousWidth / codeBlockMinWidth) * typingTime;
-
-  const animationDuration = (currentWidth / codeBlockMinWidth) * typingTime;
-  rect.style.opacity = '0';
-
-  rect.style.animation = `typing ${animationDuration}s ${delayTime}s linear forwards`;
-
-  currentDelay = delayTime;
-  previousRect = rect;
-}
+blocks.animate('circle', svg, 0.2);
 
 output?.appendChild(svg);
