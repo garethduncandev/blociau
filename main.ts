cbe6b2458386eae2bfd4da7370b5675a11b742b2
Gareth Duncan <gareth.duncan@fft.org.uk>
2023-10-23 14:03:34 +0100

    added border radious to svg rect

--- main.ts
+++ main.ts
@@ -1,41 +1,41 @@
+import { BlockStyle } from './src/blockStyle';
 import Blocks from './src/blocks';
 
 const codeBlockHeight = 10;
 const codeBlockMinWidth = codeBlockHeight;
 const padding = codeBlockHeight / 4;
-const b: { width: number; color: string }[] = [];
+const borderRadius = 2;
+const blockStyles: BlockStyle[] = [
+  {
+    width: codeBlockMinWidth,
+    color: 'red',
+    borderRadius: borderRadius,
+  },
+  {
+    width: codeBlockMinWidth,
+    color: 'blue',
+    borderRadius: borderRadius,
+  },
+  {
+    width: codeBlockMinWidth * 2,
+    color: 'yellow',
+    borderRadius: borderRadius,
+  },
+  {
+    width: codeBlockMinWidth * 3,
+    color: 'green',
+    borderRadius: borderRadius,
+  },
+  {
+    width: codeBlockMinWidth * 3,
+    color: 'purple',
+    borderRadius: borderRadius,
+  },
+];
 
-const blocks = new Blocks(
-  codeBlockHeight,
-  [
-    {
-      width: codeBlockMinWidth,
-      color: 'red',
-    },
-    {
-      width: codeBlockMinWidth,
-      color: 'blue',
-    },
-    {
-      width: codeBlockMinWidth * 2,
-      color: 'yellow',
-    },
-    {
-      width: codeBlockMinWidth * 3,
-      color: 'green',
-    },
-    {
-      width: codeBlockMinWidth * 3,
-      color: 'purple',
-    },
-  ],
-
-  padding
-);
+const blocks = new Blocks(codeBlockHeight, blockStyles, padding);
 
 const img = document.getElementById('input') as HTMLImageElement;
 const output = document.getElementById('output');
-
 const svg = blocks.create('circle', img);
-
 output?.appendChild(svg);
--- src/blockStyle.ts
+++ src/blockStyle.ts
@@ -1,4 +1,5 @@
 export interface BlockStyle {
   width: number;
   color: string;
+  borderRadius: number;
 }
